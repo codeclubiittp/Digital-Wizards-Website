71968f55cb6325e269a0caa4b8771eba621c7f4f
amitjomy007 <amitjomy007@gmail.com>
2024-09-06 01:31:47 +0530

    added card component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar.js'; 
 import Footer from '../components/Footer.js';
+import Card from '../components/card-component.jsx'
 
 function HomePage() {
   return (
@@ -10,6 +11,7 @@ function HomePage() {
       {/* <h1>Home Page</h1> */}
       {/* <Link to="/about">Go to About Page</Link> */}
       {/* <Footer/> */}
+      <Card />
     </div>
   );
 }
