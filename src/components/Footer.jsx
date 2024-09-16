6153f287a3091823039f01cf16b372f064ff410b
ArthPatel02 <iarthpatel1405@gmail.com>
2024-09-16 13:17:17 +0530

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import '../css/NavBar.css'; // Import CSS for styling
+import '../css/Footer.css'; // Import CSS for styling
 
 function Footer(){
     return (
-    <div className='footer'>Footer</div>);
+        <footer>
+        <div class="footer-container">
+            <div class="footer-left">
+                
+                <div class="logo"></div>
+                {/* Add DW Logo */}
+                <p>Digital Wizards</p>
+            </div>
+    
+            <div class="footer-links">
+                <ul>
+                    <li>ABOUT US</li>
+                    <li>DOMAINS</li>
+                    <li>CONTACT US</li>
+                </ul>
+    
+                <ul>
+                    {/* Add Social icons + Links*/}
+                    <li>SOCIAL 1</li>
+                    <li>SOCIAL 2</li>
+                    <li>SOCIAL 3</li>
+                </ul>
+            </div>
+    
+            <div class="footer-bottom">
+                <p>&copy; Copyright. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+    );
 }
 
 export default Footer;
