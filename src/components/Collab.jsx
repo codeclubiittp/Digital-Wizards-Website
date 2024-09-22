42d6b8e081c71f6cf9a76f1e37c91b4a8fb6b62d
ArthPatel02 <iarthpatel1405@gmail.com>
2024-09-22 12:15:08 +0530

    Collab added - (Few viewports not fixed)

---
+++ src/components/Collab.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import '../css/Collab.css'; // Import CSS for styling
+
+function Collab(){
+    return (
+        <div class="container">
+            <div class="collab-left">
+                <div class="collab-left-div">
+                <p id="heading">Our Top Collaborations<hr></hr></p>
+                <br></br>
+                <p id="heading-extra">Partnering with leading clients and collaborators to inspire innovation and achieve enduring success.</p>
+                </div>
+            </div>
+                <div class="logos-section">
+                    <div class="collab-right-div">
+                        <img src="https://www.urbanfoodies.com.au/wp-content/uploads/2019/07/microsoft-logo-white-transparent_2166875-300x110.png" alt=""></img>
+                        <img src="https://static.vecteezy.com/system/resources/previews/026/135/317/non_2x/meta-social-media-logo-symbol-with-name-black-design-illustration-free-vector.jpg" alt=""></img>
+                        <img src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo-768x432.png" alt=""></img>
+                        <img src="https://static.vecteezy.com/system/resources/previews/026/135/317/non_2x/meta-social-media-logo-symbol-with-name-black-design-illustration-free-vector.jpg" alt=""></img>
+                        <img src="https://investlithuania.com/wp-content/uploads/Cognizant.svg" alt=""></img>
+                        <img src="https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/d28dc694-245b-4284-9c94-0150ceaa81f5.png" alt=""></img>
+                    </div>
+            </div>
+    </div>
+    );
+}
+
+export default Footer;
