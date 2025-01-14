15443c28e7be09ff0f6d5da479de6e2fb62314a3
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2025-01-14 12:39:57 +0530

    Fix: Card images not properly positioned for mobile

--- src/components/Domains.jsx
+++ src/components/Domains.jsx
@@ -6,32 +6,32 @@ const domains = [
   {
     title: 'App Development',
     description: 'Creating applications for mobile and desktop platforms.',
-    backgroundImage: 'url("/images/domainLogos/App_Negative.png")'
+    backgroundImage: '/images/domainLogos/App_Negative.png'
   },
   {
     title: 'Architecture',
     description: 'Analyzing and interpreting complex data to drive decisions.',
-    backgroundImage: 'url("/images/domainLogos/C.Arch_Negative.png")'
+    backgroundImage: '/images/domainLogos/C.Arch_Negative.png'
   },
   {
     title: 'Competitive Programming',
     description: 'Mastering problem-solving and algorithmic thinking through coding challenges under time and resource constraints.',
-    backgroundImage: 'url("/images/domainLogos/CP_Negative.png")'
+    backgroundImage: '/images/domainLogos/CP_Negative.png'
   },
   {
     title: 'Cyber Security',
     description: 'Protecting systems and networks from digital attacks.',
-    backgroundImage: 'url("/images/domainLogos/Cyber_Negative.png")'
+    backgroundImage: '/images/domainLogos/Cyber_Negative.png'
   },
   {
     title: 'Software Engineering',
     description: 'Designing, developing, and testing software systems.',
-    backgroundImage: 'url("/images/domainLogos/DevOps_Negative.png")'
+    backgroundImage: '/images/domainLogos/DevOps_Negative.png'
   },
   {
     title: 'Web Development',
     description: 'Building and maintaining websites and web applications.',
-    backgroundImage: 'url("/images/domainLogos/WebDevLogo.png")'
+    backgroundImage: '/images/domainLogos/WebDevLogo.png'
   }
 ];
 
@@ -53,20 +53,16 @@ const DomainCards = () => {
           // Link to navigate to the specific domain page
           <Link to={`/domain/${domain.title.toLowerCase().replace(/\s+/g, '-')}`} key={index}>
             <div
-              className="relative p-4 rounded-lg transition transform hover:scale-105"
-              style={{
-                backgroundImage: domain.backgroundImage,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center -100px',
-              }}
+              className="relative p-4 rounded-lg transition transform hover:scale-105 overflow-hidden"
             >
+            <img className="scale-[200%] translate-y-1/3" src = {domain.backgroundImage} alt = {domain.title}/>
               {/* Overlay to darken the background image */}
               <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg"></div>
 
               {/* Content inside the card */}
               <div className="relative z-10 flex flex-col items-center text-center text-white">
                 {/* Domain Title */}
-                <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2 mt-[120px]">{domain.title}</h2>
+                <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2">{domain.title}</h2>
                 {/* Domain Description */}
                 <p className="text-sm sm:text-base font-medium mb-4 px-2">{domain.description}</p>
               </div>
